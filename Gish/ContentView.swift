0b218cadfcef5b741450dec440cd3e895d62b794
Kushal <kushal.ashok@gmail.com>
2019-06-16 02:39:36 +0800

    Initial Commit

---
+++ Gish/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Gish
+//
+//  Created by Kushal Ashok on 2019/6/16.
+//  Copyright © 2019 Kushal Ashok. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif

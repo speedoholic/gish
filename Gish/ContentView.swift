ebbfd9efa44a7165023c3ea65bed6001f1ce7eed
Kushal <kushal.ashok@gmail.com>
2019-06-16 03:37:03 +0800

    Initial commit

--- Gish/ContentView.swift
+++ Gish/ContentView.swift
@@ -9,8 +9,48 @@
 import SwiftUI
 
 struct ContentView : View {
+    
+    @State private var username: String = "Guest"
+    @State private var selectedRoleIndex: Int = 0
+    
+    private var selectedRole: Role {
+        return Role(rawValue: selectedRoleIndex) ?? Role.timer
+    }
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                TextField($username)
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                segmentedView
+                
+                Divider()
+                
+                Text(selectedRole.getDescription())
+                    .font(.callout)
+                    .padding()
+                
+                Spacer()
+            }
+            .background(Color(white: 0.85))
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarTitle(Text("Gish"))
+        }
+    }
+    
+    private var segmentedView: some View {
+        SegmentedControl(selection: $selectedRoleIndex) {
+            ForEach(0..<Role.allCases.count) { index in
+                Text(Role.getString(index)).tag(index)
+            }
+        }   .font(.subheadline)
+            .padding()
     }
 }
 

5d3d01ae3a7fe2a52d267d5733dd1544bb7087df
Kushal Ashok <kushal.ashok@gmail.com>
2019-07-05 13:10:44 +0800

    Set category

--- Gish/Views/ContentView.swift
+++ Gish/Views/ContentView.swift
@@ -57,7 +57,7 @@ struct ContentView : View {
                     ZStack {
                         Circle()
                             .fill(Color.white)
-                            .frame(width: 150, height: 150)
+                            .frame(width: 100, height: 100)
                         Text("Go")
                             .font(.largeTitle)
                     }
